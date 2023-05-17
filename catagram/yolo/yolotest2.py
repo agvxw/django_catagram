296769148e9775a3af1731e2312b82bc5a71f360
agvxw <42981315+agvxw@users.noreply.github.com>
2023-05-17 22:53:46 +0700

    Update yolotest2.py

--- catagram/yolo/yolotest2.py
+++ catagram/yolo/yolotest2.py
@@ -2,7 +2,7 @@ import torch
 import cv2
 
 name =[]
-myfile = open("django_catagram/catagram/yolo/config/coco.names", "r")
+myfile = open("coco.names", "r")
 for i in myfile:
     line = str(i)
     line = line.strip('\n')
