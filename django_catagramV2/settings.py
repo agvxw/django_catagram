92e611d65f834c21123b76524054a7012c6bd24c
agvxw <42981315+agvxw@users.noreply.github.com>
2023-05-18 17:53:53 +0700

    Update settings.py

--- django_catagramV2/settings.py
+++ django_catagramV2/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-6@qb#gd^z$$x#j2x-fzbzh+9p4-zbbvidtb61(6r1$mu)55!ch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
